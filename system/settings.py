6ff06ee660884e94e7a808d31ed6d45291a4aabc
Brayan opiyo <67815971+opiyobrayan@users.noreply.github.com>
2023-01-04 06:42:06 +0300

    Update settings.py

--- system/settings.py
+++ system/settings.py
@@ -135,3 +135,6 @@ STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CSRF_TRUSTED_ORIGINS=['https://*system.up.railway.app/']
